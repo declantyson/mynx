fa8bac2ad8c78b3e755377814e0451b6453b13d2
Declan Tyson <declantyson@hotmail.co.uk>
2013-03-25 17:39:57 +0000

    we now have skeleton content management
    for anything with an id of 1

---
+++ admin/editpage.aspx
@@ -0,0 +1,44 @@
+<%@ Page Title="" Language="C#" MasterPageFile="/themes/mundane/master.master" AutoEventWireup="true" CodeFile="editpage.aspx.cs" Inherits="Page" ValidateRequest="false" %>
+
+<asp:Content ID="content" ContentPlaceHolderID="content" Runat="Server">
+
+	<title>Edit "<%= pageTitle %>"</title>
+
+	<form id="pageForm" class="edit" style="padding: 1%" method="post" runat="server">
+		<h3>Edit "<%= pageTitle %>"</h3>
+		<div>
+			<label for="title" style="width:100px;display:inline-block">Title</label>
+			<input name="title" type="text" value="<%= pageTitle %>"/>
+			<div style="clear:both;"></div>
+
+			<label for="slug" style="width:100px;display:inline-block">Slug</label>
+			<input name="slug" type="text" value="<%= slug %>"/>
+			<div style="clear:both;"></div>
+			
+			<input type="hidden" name="id" value="<%= id %>" />
+			<input id="text" type="hidden" name="text" value="<%= text %>" />
+
+        	<asp:Button id="update_blog" runat="Server" Text="Update" OnClientClick="return rasterizeContent();" onClick="update_page"/>
+		</div>
+	</form>
+
+	<div class="clearfix"></div>
+
+	<div class="widget-col col-33 new-col main-widget-editor">
+		<p class="new-content-box">Add content box</p>
+		<p class="new-html">Add HTML outside of content box</p>
+	</div>
+	<div class="clearfix"></div>
+
+	<div class="editable-content">
+
+		<%= text %>
+
+	</div>
+
+	<div class="widget-col col-33 new-col main-widget-editor">
+		<p class="new-content-box">Add content box</p>
+		<p class="new-html">Add HTML outside of content box</p>
+	</div>
+
+</asp:Content>
---
+++ admin/editpage.aspx.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.Web;
+using System.IO;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+public partial class Page : System.Web.UI.Page
+{
+	public string currentTheme = ""; // this will be defined in the database later on
+	public string pageTitle = "";
+	public string slug = "";
+	public string text = "";
+	public string id = "";
+
+	protected void Page_PreInit(object sender, EventArgs e)	{
+		SqlConnection connection = new SqlConnection(GetConnectionString());
+		string sql_string = "SELECT TOP 1 * FROM settings";
+        try	{
+			connection.Open();
+			SqlDataReader settings_reader = null;
+			SqlCommand sql_command = new SqlCommand(sql_string, connection);
+			settings_reader = sql_command.ExecuteReader();
+			
+			while(settings_reader.Read())
+			{	
+				currentTheme = settings_reader["current_theme"].ToString();
+			}
+	
+		} catch (System.Data.SqlClient.SqlException ex) {
+			string msg = "D'oh, something's not right...";
+			msg += ex.Message;
+			throw new Exception(msg);
+		} finally {
+			connection.Close();
+		}
+
+	    MasterPageFile = "/themes/" + currentTheme + "/admin.master";
+	}
+
+    protected void Page_Load(object sender, EventArgs e) {        
+		SqlConnection connection = new SqlConnection(GetConnectionString());
+		string sql_string = "SELECT * FROM pages WHERE id = 1";
+        try	{
+			connection.Open();
+			SqlDataReader page_reader = null;
+			SqlCommand sql_command = new SqlCommand(sql_string, connection);
+			page_reader = sql_command.ExecuteReader();
+			
+			while(page_reader.Read())
+			{	
+				pageTitle = page_reader["title"].ToString();
+				id = page_reader["id"].ToString();
+				slug = page_reader["slug"].ToString();
+				text = page_reader["text"].ToString();
+			}
+
+			text = text.Replace("\"", "'");
+			text = text.Trim();
+	
+		} catch (System.Data.SqlClient.SqlException ex)	{
+			string msg = "D'oh, something's not right...";
+			msg += ex.Message;
+			throw new Exception(msg);
+		} finally {
+			connection.Close();
+		}
+    }
+
+    protected void update_page(object sender, EventArgs e) {
+    	SqlConnection connection = new SqlConnection(GetConnectionString());
+
+		try {
+            connection.Open();
+			using (SqlCommand cmd =new SqlCommand("UPDATE pages SET title=@pageTitle,slug=@slug,text=@text WHERE id=" + Request["id"], connection))
+			{
+			cmd.Parameters.AddWithValue("@pageTitle", Request["title"]);
+			cmd.Parameters.AddWithValue("@slug", Request["slug"]);
+			cmd.Parameters.AddWithValue("@text", Request["text"]);
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.ExecuteNonQuery();
+			}
+        } catch (System.Data.SqlClient.SqlException ex) {
+            string msg = "=( Something's not right! ";
+            msg += ex.Message;
+            throw new Exception(msg);
+        } finally {
+            connection.Close();
+            Response.Redirect("/admin/editpage.aspx");
+        }
+    }
+	
+	public string GetConnectionString() {
+       return System.Configuration.ConfigurationManager.ConnectionStrings["mynxConnectionString"].ConnectionString;
+    } 
+}
